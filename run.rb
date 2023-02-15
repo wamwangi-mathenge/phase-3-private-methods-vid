f19cf37cf19164aebc6695f61c6926fac66ae31f
Brian <wamwangimathenge@gmail.com>
2023-02-15 10:06:43 +0300

    Class method

--- run.rb
+++ run.rb
@@ -2,12 +2,19 @@ class Bartender
 
     attr_accessor :name
 
+    @@all = []
+
     def initialize(name)
         @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
     end
 
     def intro
-        "Hello, my name is #{self.name}"
+        "Hello, my name is #{self.name}!"
     end
 end
 
