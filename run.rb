65074e88aef4e56888c58b0794b58c9f8245e90a
Brian <wamwangimathenge@gmail.com>
2023-02-15 10:25:03 +0300

    Built 3 private methods and a public method that calls on them

--- run.rb
+++ run.rb
@@ -18,6 +18,28 @@ class Bartender
     def intro
         "Hello, my name is #{self.name}!"
     end
+
+    def make_drink
+        @cocktail_ingredients = []
+        choose_liquor
+        choose_mixer
+        choose_garnish
+        "Here is your drink. It contains #{@cocktail_ingredients}"
+    end
+
+    private
+
+    def choose_liquor
+        @cocktail_ingredients << "whiskey"
+    end
+
+    def choose_mixer
+        @cocktail_ingredients << "vermouth"
+    end
+
+    def choose_garnish
+        @cocktail_ingredients << "olives"
+    end
 end
 
 phil = Bartender.new("Phil")
